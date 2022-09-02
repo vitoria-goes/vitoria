f70c7dbc25ab45e52d759cb64341df374bed5ead
vitoria-goes <112700857+vitoria-goes@users.noreply.github.com>
2022-09-02 08:56:02 -0300

    Add files via upload

---
+++ multiplicacao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula01_soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero01, numero02, resultado;
+
+            Console.WriteLine("insira o primeiro número");
+            numero01 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("insira o segundo número");
+            numero02 = Convert.ToInt32(Console.ReadLine());
+
+            resultado = numero01 / numero02;
+
+            Console.WriteLine("seu resultado é; " + resultado);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
